3e4febad78f3ac9563203af1e32050aecf734af3
Rebecca Bompiani <rebeccabompiani@gmail.com>
2020-02-17 21:39:25 -0800

    loaded external json data, created svg shapes for each json object, and bound json data to svg shapes

--- index.js
+++ index.js
@@ -1,35 +1,22 @@
-const canvas = d3.select(".canvas");
+// select svg container
 
-const svg = canvas.append('svg')
-    .attr('height', 600)
-    .attr('width', 600);
+const svg = d3.select('svg');
 
-const group = svg.append('g')
-    .attr('transform', 'translate(0,100)');
+d3.json('planets.json').then(data => {
+    const circs = svg.selectAll('circle')
+        .data(data);
 
-//append shapes to svg container
-group.append('rect')
-    .attr('width', 200)
-    .attr('height', 100)
-    .attr('fill', 'blue')
-    .attr('x', 20)
-    .attr('y', 20);
-group.append('circle')
-    .attr('r', 50)
-    .attr('cx', 300)
-    .attr('cy', 70)
-    .attr('fill', 'pink');
-group.append('line')
-    .attr('x1', 370)
-    .attr('x2', 400)
-    .attr('y1', 20)
-    .attr('y2', 120)
-    .attr('stroke', 'red');
+    //add attributes to existing circles in the DOM
+    circs.attr('cy', 200)
+        .attr('cx', d => d.distance)
+        .attr('r', d => d.radius)
+        .attr('fill', d => d.fill);
 
-svg.append('text')
-    .attr('x', 20)
-    .attr('y', 200)
-    .attr('fill', 'grey')
-    .text('hello, ninjas')
-    .style('font-family', 'arial');
-
+    // get the enter selection and append to the dom
+    circs.enter()
+        .append('circle')
+        .attr('cy', 200)
+        .attr('cx', d => d.distance)
+        .attr('r', d => d.radius)
+        .attr('fill', d => d.fill);
+})
