0e8438ee6ed017ba9b12025abae5a91fc0c3cc34
Rebecca Bompiani <rebeccabompiani@gmail.com>
2020-03-15 17:42:21 -0700

    added simple band scaling between bars

--- index.js
+++ index.js
@@ -2,23 +2,30 @@
 
 const svg = d3.select('svg');
 
-const y = d3.scaleLinear()
-    .domain([0, 1000])
-    .range([0, 500]);
 
 d3.json('planets.json').then(data => {
+
+    const y = d3.scaleLinear()
+        .domain([0, 1000])
+        .range([0, 500]);
+
+    const x = d3.scaleBand()
+        .domain(data.map(item => item.name))
+        .range([0, 500]);
+
+
     const rects = svg.selectAll('rects')
         .data(data);
 
-    rects.attr('width', 50)
+    rects.attr('width', x.bandwidth)
         .attr('height', d => d.orders)
         .attr('fill', 'orange')
-        .attr('x', (d, i) => i * 70);
+        .attr('x', d => x(d.name));
 
     rects.enter()
         .append('rect')
-        .attr('width', 50)
+        .attr('width', x.bandwidth)
         .attr('height', d => d.orders)
         .attr('fill', 'orange')
-        .attr('x', (d, i) => i * 70);
+        .attr('x', d => x(d.name));
 })
