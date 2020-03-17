f178161966b6997d199ddc3d05d87db40dbefbe2
Rebecca Bompiani <rebeccabompiani@gmail.com>
2020-03-17 12:36:09 -0700

    refactored to incorporate d3 update pattern

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* ----------- CONTAINER & GRAPH ----------- */
 // select canvas container and add svg element
 const svg = d3.select('.canvas')
     .append('svg')
@@ -18,31 +19,48 @@ const xAxisGroup = graph.append('g')
     .attr('transform', `translate(0,${graphHeight})`)
 const yAxisGroup = graph.append('g');
 
-// retrieve json data
+/* retrieve json data
 d3.json('planets.json').then(data => {
+*/
 
-    //determin min/max values from data
-    const min = d3.min(data, d => d.orders);
-    const max = d3.max(data, d => d.orders);
-    const extent = d3.extent(data, d => d.orders);
+/* ----------- SCALES & AXES ------------- */
+//remap vertical scale based on data max value
+const y = d3.scaleLinear()
+    .range([graphHeight, 0]);
 
-    //remap vertical scale based on data max value
-    const y = d3.scaleLinear()
-        .domain([0, max])
-        .range([graphHeight, 0]);
+//remap horizontal band widths based on number of entries and bar padding
+const x = d3.scaleBand()
+    .range([0, 500])
+    .paddingInner(0.2)
+    .paddingOuter(0.2);
 
-    //remap horizontal band widths based on number of entries and bar padding
-    const x = d3.scaleBand()
-        .domain(data.map(item => item.name))
-        .range([0, 500])
-        .paddingInner(0.2)
-        .paddingOuter(0.2);
+//create the axes
+const xAxis = d3.axisBottom(x);
+const yAxis = d3.axisLeft(y)
+    .ticks(3)
+    .tickFormat(d => d + ' orders');
 
+// update x axis text
+xAxisGroup.selectAll('text')
+    .attr('transform', 'rotate(-40)')
+    .attr('text-anchor', 'end')
+    .attr('fill', 'orange');
+
+/* ----------- UPDATE FUNCTION ----------- */
+const update = (data) => {
+
+    //update scale domains
+    y.domain([0, d3.max(data, d => d.orders)]);
+    x.domain(data.map(item => item.name));
 
     //select all rects within the svg(currently, none)
     const rects = graph.selectAll('rects')
         .data(data);
 
+    // remove extra exit selection rectangles
+    rects.exit().remove();
+
+    // update shapes currently in DOM
     rects.attr('width', x.bandwidth)
         .attr('height', d => y(d.orders))
         .attr('fill', 'orange')
@@ -57,17 +75,18 @@ d3.json('planets.json').then(data => {
         .attr('x', d => x(d.name))
         .attr('y', d => y(d.orders));
 
-    //create and call the axes
-    const xAxis = d3.axisBottom(x);
-    const yAxis = d3.axisLeft(y)
-        .ticks(3)
-        .tickFormat(d => d + ' orders');
-
+    // call axes
     xAxisGroup.call(xAxis);
     yAxisGroup.call(yAxis);
+}
 
-    xAxisGroup.selectAll('text')
-        .attr('transform', 'rotate(-40)')
-        .attr('text-anchor', 'end')
-        .attr('fill', 'orange');
+/* ----------- CONNECT TO FIREBASE ----------- */
+db.collection('dishes').get().then(res => {
+
+    var data = [];
+    res.docs.forEach(doc => {
+        data.push(doc.data());
+    })
+
+    update(data);
 })
