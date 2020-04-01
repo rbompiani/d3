52d30b7e3c47129d0ade1df661006cbedc0a7afa
Rebecca Bompiani <rebeccabompiani@gmail.com>
2020-04-01 11:41:06 -0700

    receiving snapshot data from firestore

--- donutChart/graph.js
+++ donutChart/graph.js
@@ -14,3 +14,38 @@ const pie = d3.pie()
     .sort(null)
     .value(d => d.cost);
 
+const arcPath = d3.arc()
+    .outerRadius(dims.radius)
+    .innerRadius(dims.radius / 2);
+
+
+//update function
+const update = () => {
+    console.log(data);
+}
+
+let data = [];
+
+db.collection('expenses').onSnapshot(res => {
+
+    res.docChanges().forEach(change => {
+        const doc = { ...change.doc.data(), id: change.doc.id };
+
+        switch (change.type) {
+            case 'added':
+                data.push(doc);
+                break;
+            case 'modified':
+                const index = data.findIndex(item => item.id == doc.id);
+                data[index] = doc;
+                break;
+            case 'removed':
+                data = data.filter(item => item.id !== doc.id);
+                break;
+            default:
+                break;
+        }
+    })
+
+    update(data);
+});
